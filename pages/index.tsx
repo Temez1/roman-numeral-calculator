8c8b5e8cf278ba37a5e038dec484b93740b55cce
Teemu Helenius <heleniusteemu@gmail.com>
2022-09-13 13:39:11 +0300

    Add number to roman numeral converter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,50 @@
 import Head from "next/head";
+import { useState } from "react";
+const romanize: Romanize = require("romanize");
+
+type Romanize = (positiveInteger: number) => string;
 
 export default function Home() {
+  const [romanNumeral, setRomanNumeral] = useState<string>();
+
+  const handleInputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = Number(e.currentTarget.value);
+
+    if (isNaN(input)) {
+      setRomanNumeral(undefined);
+      return;
+    }
+
+    const integer = Math.round(input);
+    if (integer < 1 || integer > 1000) {
+      setRomanNumeral(undefined);
+      return;
+    }
+
+    setRomanNumeral(romanize(integer));
+  };
+
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+    >
       <Head>
-        <title>Create Next App</title>
+        <title>Roman Numeral Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1>Roman Numeral Calculator</h1>
+        <h3>Convert number to Roman Numeral</h3>
+
+        <input onChange={handleInputOnChange}></input>
+
+        <h4>{romanNumeral || "Waiting for number between 1 and 1000"}</h4>
       </main>
     </div>
   );
