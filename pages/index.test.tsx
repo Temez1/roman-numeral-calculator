d86a4db76bd17f6cc8214cc0168842a33c5f475c
Teemu Helenius <heleniusteemu@gmail.com>
2022-09-14 13:58:49 +0300

    Add tests

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,14 +1,63 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
+import userEvent from "@testing-library/user-event";
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+describe("Home", () => {
+  it("renders waiting for number between 1 and 1000", () => {
+    render(<Home />);
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+    const text = screen.getByText(/Waiting for number between 1 and 1000/i);
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(text).toBeInTheDocument();
+  });
+  it("the default text stays unchanged after typing text", async () => {
+    const user = userEvent.setup();
+    render(<Home />);
+
+    const inputElement = screen.getByRole("textbox");
+    await user.type(inputElement, "asdf");
+
+    const text = screen.getByText(/Waiting for number between 1 and 1000/i);
+    expect(text).toBeInTheDocument();
+  });
+  it("the default text stays unchanged after typing number below 1", async () => {
+    const user = userEvent.setup();
+    render(<Home />);
+
+    const inputElement = screen.getByRole("textbox");
+    await user.type(inputElement, "0");
+
+    const text = screen.getByText(/Waiting for number between 1 and 1000/i);
+    expect(text).toBeInTheDocument();
+  });
+  it("the default text stays unchanged after typing number above 1000", async () => {
+    const user = userEvent.setup();
+    render(<Home />);
+
+    const inputElement = screen.getByRole("textbox");
+    await user.type(inputElement, "1001");
+
+    const text = screen.getByText(/Waiting for number between 1 and 1000/i);
+    expect(text).toBeInTheDocument();
+  });
+  it("Renders Roman numeral 'CXXIII' after typing number 123", async () => {
+    const user = userEvent.setup();
+    render(<Home />);
+
+    const inputElement = screen.getByRole("textbox");
+    await user.type(inputElement, "123");
+
+    const text = screen.getByText(/CXXIII/i);
+    expect(text).toBeInTheDocument();
+  });
+  it("Renders Roman numeral 'XCIX' after typing decimal number 99.4", async () => {
+    const user = userEvent.setup();
+    render(<Home />);
+
+    const inputElement = screen.getByRole("textbox");
+    await user.type(inputElement, "99.4");
+
+    const text = screen.getByText(/XCIX/i);
+    expect(text).toBeInTheDocument();
+  });
+});
